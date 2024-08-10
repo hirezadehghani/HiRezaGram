e52999761281a1087a01c0fa72993eb4511e2553
= <75562501+hirezadehghani@users.noreply.github.com>
2024-08-10 21:27:11 +0330

    feat: add server and clients files

---
+++ server/main.py
@@ -0,0 +1,11 @@
+import socket
+
+class ChatServer:
+    def __init__(self) -> None:
+        self.clients = []
+        self.nick_names = []
+        self.connections = []
+        
+
+    def connection() -> None:
+        socket;
